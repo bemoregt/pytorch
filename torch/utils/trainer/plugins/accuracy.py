1f5c7273dd1940a33a829c74b417f4c6c6ecf8c2
Gromit Park <bemore.one@gmail.com>
2017-09-01 15:34:23 +0900

    Update accuracy.py
    코멘트 상세화
    9/1

--- torch/utils/trainer/plugins/accuracy.py
+++ torch/utils/trainer/plugins/accuracy.py
@@ -9,16 +9,16 @@ class AccuracyMonitor(Monitor):
     
     # 생성자
     def __init__(self, *args, **kwargs):
-        kwargs.setdefault('unit', '%')          
-        kwargs.setdefault('precision', 2)
-        super(AccuracyMonitor, self).__init__(*args, **kwargs)
+        kwargs.setdefault('unit', '%')          # 단위 퍼센트가 디폴트        
+        kwargs.setdefault('precision', 2)       # 프리시젼은 2가 디폴트
+        super(AccuracyMonitor, self).__init__(*args, **kwargs)      # ??
         
     # 어큐러시 리턴 메써드
     def _get_value(self, iteration, input, target, output, loss):
-        batch_size = input.size(0)
-        predictions = output.max(1)[1].type_as(target)
-        correct = predictions.eq(target)
-        if not hasattr(correct, 'sum'):
+        batch_size = input.size(0)                          # 배치 사이즈
+        predictions = output.max(1)[1].type_as(target)      # 예측값을 클래스명으로
+        correct = predictions.eq(target)                    # 예측값이 타겟값과 같은지
+        if not hasattr(correct, 'sum'):                     # ??
             correct = correct.cpu()
-        correct = correct.sum()
-        return 100. * correct / batch_size
+        correct = correct.sum()                             # 맞은것 더하기
+        return 100. * correct / batch_size                  # 맞은것 평균 퍼센트로 리턴.    
